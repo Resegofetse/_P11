ea55a9e9775baf32b07f73afb535b11a436aba86
resegofetse <resegofetsepanana11@gmail.com>
2022-11-16 13:57:01 +0200

    Add project files.

---
+++ S218026867_P2022A_P11/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_bwdestination
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_input = New System.Windows.Forms.Button()
+        Me.btn_rating = New System.Windows.Forms.Button()
+        Me.btn_highest = New System.Windows.Forms.Button()
+        Me.btn_total = New System.Windows.Forms.Button()
+        Me.grd_destination = New UJGrid.UJGrid()
+        Me.btn_ratio = New System.Windows.Forms.Button()
+        Me.btn_setup = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_input
+        '
+        Me.btn_input.Location = New System.Drawing.Point(12, 61)
+        Me.btn_input.Name = "btn_input"
+        Me.btn_input.Size = New System.Drawing.Size(75, 23)
+        Me.btn_input.TabIndex = 0
+        Me.btn_input.Text = "Input"
+        Me.btn_input.UseVisualStyleBackColor = True
+        '
+        'btn_rating
+        '
+        Me.btn_rating.Location = New System.Drawing.Point(12, 238)
+        Me.btn_rating.Name = "btn_rating"
+        Me.btn_rating.Size = New System.Drawing.Size(75, 23)
+        Me.btn_rating.TabIndex = 1
+        Me.btn_rating.Text = "Rating"
+        Me.btn_rating.UseVisualStyleBackColor = True
+        '
+        'btn_highest
+        '
+        Me.btn_highest.Location = New System.Drawing.Point(12, 190)
+        Me.btn_highest.Name = "btn_highest"
+        Me.btn_highest.Size = New System.Drawing.Size(75, 23)
+        Me.btn_highest.TabIndex = 2
+        Me.btn_highest.Text = "Highest"
+        Me.btn_highest.UseVisualStyleBackColor = True
+        '
+        'btn_total
+        '
+        Me.btn_total.Location = New System.Drawing.Point(12, 100)
+        Me.btn_total.Name = "btn_total"
+        Me.btn_total.Size = New System.Drawing.Size(75, 23)
+        Me.btn_total.TabIndex = 3
+        Me.btn_total.Text = "Total"
+        Me.btn_total.UseVisualStyleBackColor = True
+        '
+        'grd_destination
+        '
+        Me.grd_destination.FixedCols = 1
+        Me.grd_destination.FixedRows = 1
+        Me.grd_destination.Location = New System.Drawing.Point(132, 21)
+        Me.grd_destination.Name = "grd_destination"
+        Me.grd_destination.Scrollbars = System.Windows.Forms.ScrollBars.Both
+        Me.grd_destination.Size = New System.Drawing.Size(445, 240)
+        Me.grd_destination.TabIndex = 4
+        '
+        'btn_ratio
+        '
+        Me.btn_ratio.Location = New System.Drawing.Point(12, 144)
+        Me.btn_ratio.Name = "btn_ratio"
+        Me.btn_ratio.Size = New System.Drawing.Size(75, 23)
+        Me.btn_ratio.TabIndex = 5
+        Me.btn_ratio.Text = "Ratio"
+        Me.btn_ratio.UseVisualStyleBackColor = True
+        '
+        'btn_setup
+        '
+        Me.btn_setup.Location = New System.Drawing.Point(12, 21)
+        Me.btn_setup.Name = "btn_setup"
+        Me.btn_setup.Size = New System.Drawing.Size(75, 23)
+        Me.btn_setup.TabIndex = 6
+        Me.btn_setup.Text = "Set up"
+        Me.btn_setup.UseVisualStyleBackColor = True
+        '
+        'frm_bwdestination
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btn_setup)
+        Me.Controls.Add(Me.btn_ratio)
+        Me.Controls.Add(Me.grd_destination)
+        Me.Controls.Add(Me.btn_total)
+        Me.Controls.Add(Me.btn_highest)
+        Me.Controls.Add(Me.btn_rating)
+        Me.Controls.Add(Me.btn_input)
+        Me.Name = "frm_bwdestination"
+        Me.Text = "BWDESTINATION"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btn_input As Button
+    Friend WithEvents btn_rating As Button
+    Friend WithEvents btn_highest As Button
+    Friend WithEvents btn_total As Button
+    Friend WithEvents grd_destination As UJGrid.UJGrid
+    Friend WithEvents btn_ratio As Button
+    Friend WithEvents btn_setup As Button
+End Class
---
+++ S218026867_P2022A_P11/Form1.vb
@@ -0,0 +1,146 @@
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+Public Class frm_bwdestination
+    ' PANANA R
+    'Q1
+    '1.1 Activity record structure
+    Private Structure ActivityRec
+        Public Name As String
+        Public NumVisitors As Integer
+        Public NumPhotos() As Integer 'nS
+        Public Total As Integer
+        Public Ratio As Double
+
+    End Structure
+    '1.2 Destination record structure
+    Private Structure DestinationRec
+        Public Name As String
+        Public Activities() As ActivityRec 'nA
+        Public Index As Integer
+        Public Rating As String
+    End Structure
+    'declaring variables
+    Private nD As Integer
+    Private nA As Integer
+    Private nS As Integer
+    Private Photos() As DestinationRec   'nD
+    'setting up ujgrid
+    Private Sub PT(ByVal r As Integer, ByVal c As Integer, ByVal t As String)
+        grd_destination.Row = r
+        grd_destination.Col = c
+        grd_destination.Text = t
+    End Sub
+    'labelling ujgrid
+    Private Sub Setup()
+        nD = CInt(InputBox("Enter number of destinations"))
+        nA = CInt(InputBox("Enter number of activities"))
+        nS = CInt(InputBox("Enter number of social media sites"))
+        ReDim Photos(nD)
+        For i As Integer = 1 To nD
+            ReDim Photos(i).Activities(nA)
+            For x As Integer = 1 To nA
+                ReDim Photos(i).Activities(x).NumPhotos(nS)
+            Next x
+            grd_destination.Rows = nD + 1
+            grd_destination.Cols = nA + 1 + 2
+        Next i
+        For d As Integer = 1 To nD
+            For a As Integer = 1 To nA
+                PT(d, 0, "Destination # " & CStr(d))
+                PT(0, a, "Activity # " & CStr(a))
+            Next a
+
+        Next d
+
+        PT(0, nA + 1, " Highest Ratio")
+        PT(0, nA + 2, "Rating")
+
+    End Sub
+
+    Private Sub btn_setup_Click(sender As Object, e As EventArgs) Handles btn_setup.Click
+        Setup()
+    End Sub
+    'prompting the user to input information of each destinatin including activity
+    Private Sub btn_input_Click(sender As Object, e As EventArgs) Handles btn_input.Click
+        For d As Integer = 1 To nD
+            Photos(d).Name = InputBox("Enter Name of destination" & CStr(d))
+            For a As Integer = 1 To nA
+                Photos(d).Activities(a).Name = InputBox("Enter name of  Activity" & CStr(a))
+                Photos(d).Activities(a).NumVisitors = CInt(InputBox("Enter number of visitors"))
+                For s As Integer = 1 To nS
+                    Photos(d).Activities(a).NumPhotos(s) = CInt(InputBox("Enter number of photos taken"))
+                Next s
+            Next a
+        Next d
+    End Sub
+    'Calculating  Total Photographs per Activity per Destination
+    Private Sub btn_total_Click(sender As Object, e As EventArgs) Handles btn_total.Click
+
+        For d As Integer = 1 To nD
+            For a As Integer = 1 To nA
+                Photos(d).Activities(a).Total = 0
+                For s As Integer = 1 To nS
+                    Photos(d).Activities(a).Total = Photos(d).Activities(a).Total + Photos(d).Activities(a).NumPhotos(s)
+                    PT(d, a, ((CStr(Photos(d).Activities(a).NumPhotos(s)))))
+                Next s
+
+            Next a
+        Next d
+    End Sub
+    'creating a subroutine called CalcRatio which will return a double
+    Private Sub CalcRatio(ByVal a As Integer, ByVal b As Integer, ByRef answer As Double)
+        answer = a / b
+
+    End Sub
+    'Calculating  Photos-Visitors ratio per Activity per Destination using CalcRatio subroutine
+    Private Sub btn_ratio_Click(sender As Object, e As EventArgs) Handles btn_ratio.Click
+        Dim total As Integer
+        For d As Integer = 1 To nD
+            total = 0
+            For a As Integer = 1 To nA
+                total = total + Photos(d).Activities(a).Total
+                CalcRatio(total, Photos(d).Activities(a).NumVisitors, Photos(d).Activities(a).Ratio)
+                'displaying the ratio on the uj grid
+                PT(d, a, (Format((Photos(d).Activities(a).Ratio), "0.00")))
+            Next a
+
+        Next d
+    End Sub
+    'Calculating Index of Activity with highest Photos-Visitors Ratio per Destination
+    Private Sub btn_highest_Click(sender As Object, e As EventArgs) Handles btn_highest.Click
+
+        Dim max As Double = 0
+        Dim Index As Integer = 0
+        For d As Integer = 1 To nD
+            For a As Integer = 1 To nA
+                If Photos(d).Activities(a).Ratio > max Then
+                    max = Photos(d).Activities(a).Ratio
+                    Index = a
+                End If
+                'Display Index of Activity with highest Photos-Visitors Ratio per Destination on the uj grid
+                PT(d, nA + 1, (CStr(Index)))
+            Next a
+
+
+        Next d
+    End Sub
+    'Calculate Tourism Rating for each Destination
+    Private Sub btn_rating_Click(sender As Object, e As EventArgs) Handles btn_rating.Click
+        Dim response As String
+        For d As Integer = 1 To nD
+            For a As Integer = 1 To nA
+                Dim Value As Double = Photos(d).Activities(a).Ratio Mod 2
+                Select Case Value
+                    Case Is = 0
+                        response = "A"
+                    Case Else
+                        response = "B"
+                End Select
+                PT(d, nA + 2, response)
+            Next a
+
+        Next d
+
+    End Sub
+End Class
